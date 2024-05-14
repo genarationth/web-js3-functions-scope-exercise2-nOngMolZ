db911f6d808617c5276e0f4af1f2fd03c01e24c8
nOngMOlZ <redem.mnd@gmail.com>
2024-05-14 15:42:27 +0700

    update ex

--- app.js
+++ app.js
@@ -4,5 +4,3 @@ const isEven = (num) => num % 2 === 0;
 console.log(isEven(4)); 
 console.log(isEven(7)); 
 
-// Exercise 2.2 converts the function from Exercise 2.1 into an arrow function
-const iSEven = (num) => num % 2 === 0;
