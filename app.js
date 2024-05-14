fe90ab63954b49a9fa85b8e4643decfb77ac4f4f
nOngMOlZ <redem.mnd@gmail.com>
2024-05-14 15:37:05 +0700

    WEB-JS3-functions-scope-exercise2

---
+++ app.js
@@ -0,0 +1,8 @@
+const isEven = (num) => num % 2 === 0;
+
+// Example
+console.log(isEven(4)); 
+console.log(isEven(7)); 
+
+// Exercise 2.2 converts the function from Exercise 2.1 into an arrow function
+const iSEven = (num) => num % 2 === 0;
